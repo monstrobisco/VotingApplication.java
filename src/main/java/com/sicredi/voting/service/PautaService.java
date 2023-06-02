e4f16f193bdb9f860ec7863564711432881d2c8e
thmedeiroslima <97640552+thmedeiroslima@users.noreply.github.com>
2023-06-01 22:02:11 -0300

    Melhorias no log da classe PautaService
    #Desafio sicredi

--- src/main/java/com/sicredi/voting/service/PautaService.java
+++ src/main/java/com/sicredi/voting/service/PautaService.java
@@ -3,16 +3,24 @@ import com.sicredi.voting.model.PautaModel;
 import com.sicredi.voting.repository.PautaRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class PautaService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PautaService.class);
     private final PautaRepository pautaRepository;
 
     public PautaService(PautaRepository pautaRepository) {
         this.pautaRepository = pautaRepository;
+        LOGGER.info("PautaService inicializado com sucesso.");
     }
 
     public Mono<PautaModel> criarPauta(PautaModel pauta) {
-        return pautaRepository.save(pauta);
+        LOGGER.info("Criando pauta: {}", pauta.getTitulo());
+        return pautaRepository.save(pauta)
+                .doOnSuccess(p -> LOGGER.info("Pauta criada com sucesso: {}", p.getTitulo()))
+                .doOnError(e -> LOGGER.error("Erro ao criar pauta: {}", pauta.getTitulo(), e));
     }
 }
