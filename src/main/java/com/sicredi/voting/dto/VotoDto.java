f8311180c2500998b6e0472d5b14ed30987f9970
thmedeiroslima <97640552+thmedeiroslima@users.noreply.github.com>
2023-06-02 00:57:25 -0300

    Configuração Swagger
    #Desafio sicredi

---
+++ src/main/java/com/sicredi/voting/dto/VotoDto.java
@@ -0,0 +1,16 @@
+package com.sicredi.voting.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VotoDto {
+
+    private String idPauta;
+    private String cpfAssociado;
+    private boolean voto;
+
+
+}
+
--- src/main/java/com/sicredi/voting/funcional/HandlerRouter.java
+++ src/main/java/com/sicredi/voting/funcional/HandlerRouter.java
@@ -1,6 +1,9 @@
 package com.sicredi.voting.funcional;
 
 
+import com.sicredi.voting.dto.PautaDto;
+import com.sicredi.voting.dto.SessaoVotacaoDto;
+import com.sicredi.voting.dto.VotoDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -22,150 +25,150 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 public class HandlerRouter {
 
     @Bean
-    @RouterOperations(
-            {
-                    @RouterOperation(
-                            path =  "/api/v1/pautas",
-                            produces = {
-                                    MediaType.APPLICATION_JSON_VALUE
-                            },
-                            method = RequestMethod.POST,
-                            beanClass = PautaHandler.class,
-                            beanMethod = "criarPauta",
-                            operation = @Operation(
-                                    operationId = "criarPauta",
-                                    responses = {
-                                            @ApiResponse(
-                                                    responseCode = "200",
-                                                    description = "Sucessfull Operation",
-                                                    content = @Content(schema = @Schema(
-                                                            implementation = PautaHandler.class
-                                                    ))
-                                            )
-                                    }
-                            )
-                    ),
-                    @RouterOperation(
-                            path =  "/api/v1/pautas/votos",
-                            produces = {
-                                    MediaType.APPLICATION_JSON_VALUE
-                            },
-                            method = RequestMethod.POST,
-                            beanClass = VotoHandler.class,
-                            beanMethod = "votar",
-                            operation = @Operation(
-                                    operationId = "votar",
-                                    responses = {
-                                            @ApiResponse(
-                                                    responseCode = "200",
-                                                    description = "Sucessfull Operation",
-                                                    content = @Content(schema = @Schema(
-                                                            implementation = VotoHandler.class
-                                                    ))
-                                            )
-                                    }
-                            )
-                    ),
-                    @RouterOperation(
-                            path =  "/api/v1/pautas/pauta/{idPauta}",
-                            produces = {
-                                    MediaType.APPLICATION_JSON_VALUE
-                            },
-                            method = RequestMethod.GET,
-                            beanClass = VotoHandler.class,
-                            beanMethod = "getVotosPauta",
-                            operation = @Operation(
-                                    operationId = "getVotosPauta",
-                                    responses = {
-                                            @ApiResponse(
-                                                    responseCode = "200",
-                                                    description = "Sucessfull Operation",
-                                                    content = @Content(schema = @Schema(
-                                                            implementation = VotoHandler.class
-                                                    ))
-                                            ),
-                                            @ApiResponse(responseCode = "404", description = "Pauta não Encontrada")
-                                    },
-                                    parameters = {
-                                            @Parameter(in = ParameterIn.PATH, name = "idPauta")
-                                    }
-                            )
-                    ),
-                    @RouterOperation(
-                            path =  "/api/v1/pautas/{idPauta}/votos",
-                            produces = {
-                                    MediaType.APPLICATION_JSON_VALUE
-                            },
-                            method = RequestMethod.GET,
-                            beanClass = VotoHandler.class,
-                            beanMethod = "getVotosPorPauta",
-                            operation = @Operation(
-                                    operationId = "getVotosPorPauta",
-                                    responses = {
-                                            @ApiResponse(
-                                                    responseCode = "200",
-                                                    description = "Sucessfull Operation",
-                                                    content = @Content(schema = @Schema(
-                                                            implementation = VotoHandler.class
-                                                    ))
-                                            ),
-                                            @ApiResponse(responseCode = "404", description = "Pauta não Encontrada")
-                                    },
-                                    parameters = {
-                                            @Parameter(in = ParameterIn.PATH, name = "idPauta")
-                                    }
-                            )
-                    ),
-                    @RouterOperation(
-                            path =  "/api/v1/sessoes",
-                            produces = {
-                                    MediaType.APPLICATION_JSON_VALUE
-                            },
-                            method = RequestMethod.POST,
-                            beanClass = SessaoVotacaoHandler.class,
-                            beanMethod = "abrirSessao",
-                            operation = @Operation(
-                                    operationId = "abrirSessao",
-                                    responses = {
-                                            @ApiResponse(
-                                                    responseCode = "200",
-                                                    description = "Sucessfull Operation",
-                                                    content = @Content(schema = @Schema(
-                                                            implementation = SessaoVotacaoHandler.class
-                                                    ))
-                                            )
-                                    }
-                            )
-                    ),
-                    @RouterOperation(
-                            path =  "/api/v1/pautas/{idPauta}/votosPostandoNaFila",
-                            produces = {
-                                    MediaType.APPLICATION_JSON_VALUE
-                            },
-                            method = RequestMethod.GET,
-                            beanClass = VotoHandler.class,
-                            beanMethod = "getVotosPorPautaPostandoNaFila",
-                            operation = @Operation(
-                                    operationId = "getVotosPorPautaPostandoNaFila",
-                                    responses = {
-                                            @ApiResponse(
-                                                    responseCode = "200",
-                                                    description = "Sucessfull Operation",
-                                                    content = @Content(schema = @Schema(
-                                                            implementation = VotoHandler.class
-                                                    ))
-                                            ),
-                                            @ApiResponse(responseCode = "404", description = "Pauta não Encontrada")
-                                    },
-                                    parameters = {
-                                            @Parameter(in = ParameterIn.PATH, name = "idPauta")
-                                    }
-                            )
+    @RouterOperations({
+            @RouterOperation(
+                    path = "/api/v1/pautas",
+                    produces = MediaType.APPLICATION_JSON_VALUE,
+                    method = RequestMethod.POST,
+                    beanClass = PautaHandler.class,
+                    beanMethod = "criarPauta",
+                    operation = @Operation(
+                            operationId = "criarPauta",
+                            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                    description = "Objeto JSON contendo os dados da pauta",
+                                    required = true,
+                                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PautaDto.class))
+                            ),
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operação realizada com sucesso",
+                                            content = @Content(schema = @Schema(implementation = PautaDto.class))
+                                    )
+                            }
                     )
-            }
-    )
+            ),
+            @RouterOperation(
+                    path = "/api/v1/sessoes",
+                    produces = MediaType.APPLICATION_JSON_VALUE,
+                    method = RequestMethod.POST,
+                    beanClass = SessaoVotacaoHandler.class,
+                    beanMethod = "abrirSessao",
+                    operation = @Operation(
+                            operationId = "abrirSessao",
+                            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                    description = "Objeto JSON contendo os dados da Sessão",
+                                    required = true,
+                                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = SessaoVotacaoDto.class))
+                            ),
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operação realizada com sucesso",
+                                            content = @Content(schema = @Schema(implementation = SessaoVotacaoDto.class))
+                                    )
+                            }
+                    )
+            ),
+            @RouterOperation(
+                    path = "/api/v1/pautas/votos",
+                    produces = MediaType.APPLICATION_JSON_VALUE,
+                    method = RequestMethod.POST,
+                    beanClass = VotoHandler.class,
+                    beanMethod = "votar",
+                    operation = @Operation(
+                            operationId = "votar",
+                            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                    description = "Objeto JSON contendo os dados do voto",
+                                    required = true,
+                                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = VotoDto.class))
+                            ),
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operação realizada com sucesso",
+                                            content = @Content(schema = @Schema(implementation = VotoDto.class))
+                                    )
+                            }
+                    )
+            ),
+            @RouterOperation(
+            path = "/api/v1/pautas/pauta/{idPauta}",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE
+            },
+            method = RequestMethod.GET,
+            beanClass = VotoHandler.class,
+            beanMethod = "getVotosPauta",
+            operation = @Operation(
+                    operationId = "getVotosPauta",
+                    responses = {
+                            @ApiResponse(
+                                    responseCode = "200",
+                                    description = "successful operation",
+                                    content = @Content(schema = @Schema(
+                                            implementation = PautaDto.class
+                                    ))
+                            ),
+                            @ApiResponse(responseCode = "404",description = "Pauta não encontrada!")
+                    },
+                    parameters = {
+                            @Parameter(in = ParameterIn.PATH,name = "idPauta")
+                    }
+            )
+    ),
+            @RouterOperation(
+                    path = "/api/v1/pautas/{idPauta}/votos",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.GET,
+                    beanClass = VotoHandler.class,
+                    beanMethod = "getVotosPorPauta",
+                    operation = @Operation(
+                            operationId = "getVotosPorPauta",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "successful operation",
+                                            content = @Content(schema = @Schema(
+                                                    implementation = PautaDto.class
+                                            ))
+                                    ),
+                                    @ApiResponse(responseCode = "404",description = "Pauta não encontrada!")
+                            },
+                            parameters = {
+                                    @Parameter(in = ParameterIn.PATH,name = "idPauta")
+                            }
+                    )
+            ),
+            @RouterOperation(
+                    path = "/api/v1/pautas/{idPauta}/votosPostandoNaFila",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.GET,
+                    beanClass = VotoHandler.class,
+                    beanMethod = "getVotosPorPautaPostandoNaFila",
+                    operation = @Operation(
+                            operationId = "getVotosPorPautaPostandoNaFila",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "successful operation",
+                                            content = @Content(schema = @Schema(
+                                                    implementation = PautaDto.class
+                                            ))
+                                    ),
+                                    @ApiResponse(responseCode = "404",description = "Pauta não encontrada!")
+                            },
+                            parameters = {
+                                    @Parameter(in = ParameterIn.PATH,name = "idPauta")
+                            }
+                    )
+            )
 
-
+    })
     public RouterFunction<ServerResponse> route(PautaHandler pautaHandler, VotoHandler votoHandler, SessaoVotacaoHandler sessaoVotacaoHandler) {
         return RouterFunctions
                 .route(RequestPredicates.POST("/api/v1/pautas").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), pautaHandler::criarPauta)
